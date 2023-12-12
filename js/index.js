633f8b802e7a605d242ddca6ddea7203a2424afe
BRUNOO1545 <brunogamer1545@gmail.com>
2023-12-11 21:47:23 -0300

    visual bug fixes

--- js/index.js
+++ js/index.js
@@ -8,15 +8,18 @@ let navOpen = false;
 
 function navHamburguerMenuShow() {
     let navMenuDisplay = document.getElementById("navMenuDisplay");
+    let navMenuContent = document.getElementById("navMenuContent");
     let navHamburguerMenuIcon = document.getElementById("navHamburguerMenuIcon");
 
     if (navOpen === true) {
         navOpen = false;
         navMenuDisplay.style.visibility = "hidden";
+        navMenuContent.style.height = 0;
         navHamburguerMenuIcon.src = "./assets/img/ui/menu_hamburguer.png";
     } else {
         navOpen = true;
         navMenuDisplay.style.visibility = "visible";
+        navMenuContent.style.height = "auto";
         navHamburguerMenuIcon.src = "./assets/img/ui/menu_hamburguer_close.png";
     }
     
@@ -27,9 +30,11 @@ function navHamburguerMenuShow() {
 
 function navHamburguerMenuForceClose() {
     let navMenuDisplay = document.getElementById("navMenuDisplay");
+    let navMenuContent = document.getElementById("navMenuContent");
     let navHamburguerMenuIcon = document.getElementById("navHamburguerMenuIcon");
 
     navMenuDisplay.style.visibility = "hidden";
+    navMenuContent.style.height = 0;
     navHamburguerMenuIcon.src = "./assets/img/ui/menu_hamburguer.png";
     navOpen = false;
     
@@ -44,5 +49,17 @@ let viewWidth = window.matchMedia("(max-width: 800px)");
 viewWidth.addEventListener("change", function() {
     navHamburguerMenuForceClose();
 });
+
+//#endregion
+
+//#region startup
+
+function pageStartup() {
+    let pageStartupDiv = document.getElementById("pageStartupDiv");
+
+    pageStartupDiv.style.visibility = "hidden";
+
+    navHamburguerMenuForceClose();
+}
 
 //#endregion
